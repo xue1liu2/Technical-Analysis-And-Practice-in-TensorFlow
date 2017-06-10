9757d6558f1e2803e80503da855ca97efc862969
xue1liu2 <1609473136@qq.com>
2017-06-10 14:10:19 +0800

    1.  数据来源　　download_and_preprocess_mscoco.sh
    １. 
    通过download_and_preprocess_mscoco.sh 获取数据文件，在download_and_preprocess_mscoco.sh，会自动调用解析数据的python文件，build_mscoco_data.py
    2.使用方法：
    在终端：进入download_and_preprocess_mscoco.sh所在的目录
    输入终端命令：sh download_and_preprocess_mscoco.sh /media/dms/a6a79dc9-900a-45ec-8255-ee2ff49eeb23/dms/Data
    注意：/media/dms/a6a79dc9-900a-45ec-8255-ee2ff49eeb23/dms/Data　该目录应该很大才是，至少右150G的空间。
    使用的时候，把/media/dms/a6a79dc9-900a-45ec-8255-ee2ff49eeb23/dms/Data换成你要存储数据的目录路径

---
+++ source/12/download_and_preprocess_mscoco.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# Script to download and preprocess the MSCOCO data set.
+# 下载和预处理数据
+#输出的是TFRecord格式的文件，build_mscoco_data.py说明了数据如何处理的。
+# The outputs of this script are sharded TFRecord files containing serialized
+# SequenceExample protocol buffers. See build_mscoco_data.py for details of how
+# the SequenceExample protocol buffers are constructed.
+#
+set -e
+
+if [ -z "$1" ]; then
+  echo "usage download_and_preproces_mscoco.sh [data dir]"
+  exit
+fi
+
+if [ "$(uname)" == "Darwin" ]; then
+  UNZIP="tar -xf"
+else
+  UNZIP="unzip -nq"
+fi
+
+
+# Create the output directories.
+OUTPUT_DIR="${1%/}"
+SCRATCH_DIR="${OUTPUT_DIR}/raw-data"
+
+mkdir -p "${OUTPUT_DIR}"
+mkdir -p "${SCRATCH_DIR}"
+CURRENT_DIR=$(pwd)
+WORK_DIR="$0.runfiles/im2txt/im2txt"
+
+
+BUILD_SCRIPT="${WORK_DIR}/build_mscoco_data"
+eval echo '$'BUILD_SCRIPT
+
+exit
+
+# Helper function to download and unpack a .zip file.
+function download_and_unzip() {
+  local BASE_URL=${1}
+  local FILENAME=${2}
+
+  if [ ! -f ${FILENAME} ]; then
+    echo "Downloading ${FILENAME} to $(pwd)"
+    wget -nd -c "${BASE_URL}/${FILENAME}"
+  else
+    echo "Skipping download of ${FILENAME}"
+  fi
+  echo "Unzipping ${FILENAME}"
+  ${UNZIP} ${FILENAME}
+}
+
+cd ${SCRATCH_DIR}
+
+# Download the images.
+BASE_IMAGE_URL="http://msvocds.blob.core.windows.net/coco2014"
+
+TRAIN_IMAGE_FILE="train2014.zip"
+download_and_unzip ${BASE_IMAGE_URL} ${TRAIN_IMAGE_FILE}
+TRAIN_IMAGE_DIR="${SCRATCH_DIR}/train2014"
+
+VAL_IMAGE_FILE="val2014.zip"
+download_and_unzip ${BASE_IMAGE_URL} ${VAL_IMAGE_FILE}
+VAL_IMAGE_DIR="${SCRATCH_DIR}/val2014"
+
+# Download the captions.
+BASE_CAPTIONS_URL="http://msvocds.blob.core.windows.net/annotations-1-0-3"
+CAPTIONS_FILE="captions_train-val2014.zip"
+download_and_unzip ${BASE_CAPTIONS_URL} ${CAPTIONS_FILE}
+TRAIN_CAPTIONS_FILE="${SCRATCH_DIR}/annotations/captions_train2014.json"
+VAL_CAPTIONS_FILE="${SCRATCH_DIR}/annotations/captions_val2014.json"
+
+
+# Build TFRecords of the image data.
+cd "${CURRENT_DIR}"
+BUILD_SCRIPT="${WORK_DIR}/build_mscoco_data"
+
+#下面这句话，执行build_mscoco_data.py文件
+"${BUILD_SCRIPT}" \
+  --train_image_dir="${TRAIN_IMAGE_DIR}" \
+  --val_image_dir="${VAL_IMAGE_DIR}" \
+  --train_captions_file="${TRAIN_CAPTIONS_FILE}" \
+  --val_captions_file="${VAL_CAPTIONS_FILE}" \
+  --output_dir="${OUTPUT_DIR}" \
+  --word_counts_output_file="${OUTPUT_DIR}/word_counts.txt" \
